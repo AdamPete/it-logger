7fc46e3f18dee07f9dfd18815b61a38563210888
AdamPete <adampetebuz@protonmail.com>
2022-01-28 11:31:50 -0600

    Added the all the modals for pj

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React, { Fragment, useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
+import AddBtn from './components/layout/AddBtn';
 import Logs from './components/logs/Logs';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 function App() {
   useEffect(() => {
@@ -13,6 +18,12 @@ function App() {
     <Fragment>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+
         <Logs />
       </div>
     </Fragment>
